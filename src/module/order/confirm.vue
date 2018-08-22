45b3086c6e78b72d8014eb1ff6bf843d022ffe4d
phantom <623601391@qq.com>
2018-08-22 08:55:37 +0800

    feat: order confirm

--- src/module/order/confirm.vue
+++ src/module/order/confirm.vue
@@ -1,7 +1,33 @@
 <template>
 	<div class="order-box">
+    <div class="address-box">
+      <van-contact-card
+        :type="cardType"
+        :name="currentContact.name"
+        :tel="currentContact.tel"
+        @click="showList = true" />
+
+      <!-- 联系人列表 -->
+      <van-popup v-model="showList" position="bottom">
+        <van-contact-list
+          v-model="chosenContactId"
+          :list="list"
+          @add="onAdd"
+          @edit="onEdit"
+          @select="onSelect"/>
+      </van-popup>
+
+      <!-- 联系人编辑 -->
+      <van-popup v-model="showEdit" position="bottom">
+        <van-contact-edit
+          :contact-info="editingContact"
+          :is-edit="isEdit"
+          @save="onSave"
+          @delete="onDelete"/>
+      </van-popup>
+    </div>
 		<div>
-			<div v-for="(shopping, index) in shoppingCartData" :key="index">
+			<div class="item" v-for="(shopping, index) in shoppingCartData" :key="index">
 				<van-card
 					:title="shopping.product.title"
 					:desc="shopping.product.title"
@@ -11,19 +37,36 @@
 					:thumb="shopping.product.cover" />
 			</div>
 		</div>
+
+    <div class="express">
+      <div class="title">配送方式</div>
+      <div class="items">
+        <span v-for="(express, index) in expresses" :key="index">
+          <van-button v-if="index === currentExpressType" @click="choiceExpressType(index)" type="danger" plain size="small">{{express.name}}</van-button>
+          <van-button v-else @click="choiceExpressType(index)" type="default" plain size="small">{{express.name}}</van-button>
+        </span>
+      </div>
+      <div class="title">配送说明</div>
+      <div class="items description">
+        {{exportDesc}}
+      </div>
+    </div>
 	</div>
 </template>
 
 
 <script>
-import { Row, Col, Button, card } from 'vant';
+import { Row, Col, Button, card, ContactCard, ContactList, ContactEdit, Popup } from 'vant';
 import Vue from 'vue';
-Vue.use(Row).use(Col).use(Button).use(card);
+Vue.use(Row).use(Col).use(Button).use(card).use(ContactCard)
+  .use(ContactList)
+  .use(Popup)
+  .use(ContactEdit);
 export default {
 	data() {
 		return {
 			nonOrders: false,
-			currentExpressType: 0,
+			currentExpressType: -1,
 			expressConfig: [],
 			shoppingCarts: [],
 			addressId: 0,
@@ -44,7 +87,18 @@ export default {
 			shoppingCartIds: '',
 			remarks: '',
 			expressConfigDescription: 'test',
-			defaultExpressDescription: '请选择合适的配送方式'
+      defaultExpressDescription: '请选择合适的配送方式',
+      chosenContactId: null,
+      editingContact: {},
+      showList: false,
+      showEdit: false,
+      isEdit: false,
+      list: [{
+        name: '张三',
+        tel: '13000000000',
+        id: 0
+      }],
+      expresses: [],
 		};
 	},
 	methods: {
@@ -52,7 +106,50 @@ export default {
 			this.$axios.get('/api/shopping/cart/', { params: params }).then((res) => {
 				this.shoppingCarts = res.data;
 			});
-		},
+    },
+    loadExpress(params) {
+      this.$axios.get('/api/express/fee', { params: params }).then((res) => {
+        this.expresses = res.data.expressFee;
+      });
+    },
+    onAdd() {
+      this.editingContact = { id: this.list.length };
+      this.isEdit = false;
+      this.showEdit = true;
+    },
+    onEdit(item) {
+      this.isEdit = true;
+      this.showEdit = true;
+      this.editingContact = item;
+    },
+    onSelect() {
+      this.showList = false;
+    },
+    choiceExpressType(index) {
+      if (index === this.currentExpressType) {
+        this.currentExpressType = -1;
+      } else {
+        this.currentExpressType = index;
+      }
+      console.log(this.currentExpressType);
+    },
+    onSave(info) {
+      this.showEdit = false;
+      this.showList = false;
+      if (this.isEdit) {
+        this.list = this.list.map(item => item.id === info.id ? info : item);
+      } else {
+        this.list.push(info);
+      }
+      this.chosenContactId = info.id;
+    },
+    onDelete(info) {
+      this.showEdit = false;
+      this.list = this.list.filter(item => item.id !== info.id);
+      if (this.chosenContactId === info.id) {
+        this.chosenContactId = null;
+      }
+    },
 	},
 	created() {
 		const query = this.$route.query;
@@ -62,18 +159,34 @@ export default {
 		this.$nextTick((res) => {
 			this.loadShoppingCarts({
 				shoppingCartIds: this.shoppingCartIds,
-			});
+      });
+      this.loadExpress({
+        shoppingCartIds: this.shoppingCartIds,
+      });
 		});
 	},
 	computed: {
 		shoppingCartData: function() {
-            return this.shoppingCarts.map((element) => {
-                return {
-                    ...element,
-                    amount: (element.amount / 100).toFixed(2),
-                };
-            });
-        },
+      return this.shoppingCarts.map((element) => {
+          return {
+              ...element,
+              amount: (element.amount / 100).toFixed(2),
+          };
+      });
+    },
+    cardType() {
+      return this.chosenContactId !== null ? 'edit' : 'add';
+    },
+    currentContact() {
+      const id = this.chosenContactId;
+      return id !== null ? this.list.filter(item => item.id === id)[0] : {};
+    },
+    exportDesc() {
+      if (this.currentExpressType >= 0) {
+        return this.expresses[this.currentExpressType].description || '暂无';
+      }
+      return '暂无';
+    }
 	}
 };
 </script>
@@ -159,13 +272,6 @@ export default {
   }
   .remark {
     margin-bottom: 40px;
-  }
-  .remark,
-  .express,
-  .value-add-service {
-    padding: 10px;
-    background-color: #ffffff;
-    font-size: 30px;
   }
   .value-add-service {
     .service-text {
@@ -270,6 +376,40 @@ export default {
 .real-amount {
   .aud-cny {
     font-size: 24px;
+  }
+}
+.order-box {
+  .item {
+    border-bottom: 2px solid #ffffff;
+  }
+  .item:last-child {
+    border: none;
+  }
+  .express {
+    margin-top: 5px;
+    background-color: #ffffff;
+    .title,
+    .items {
+      padding: 5px 10px;
+    }
+    .title {
+      font-size: 14px;
+      border-bottom: 1px #eeeeee solid;
+      padding: 10px;
+      color: #666;
+    }
+    .items {
+      span {
+        button.van-button {
+          display: inline-block;
+          margin: 2px 4px 2px 0;
+        }
+      }
+    }
+    .description {
+      color: #999;
+      font-size: 12px;
+    }
   }
 }
 </style>
